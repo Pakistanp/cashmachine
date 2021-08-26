7096409fdb40891be804f1752285ac228a797f9a
pakistanp <ppp16@vp.pl>
2021-08-26 08:51:30 +0200

    Remove comment

--- src/main/java/com/ppapierz/cashmachine/security/SecurityConfiguration.java
+++ src/main/java/com/ppapierz/cashmachine/security/SecurityConfiguration.java
@@ -23,7 +23,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").permitAll()
-                //.antMatchers("/login").permitAll()
                 .antMatchers("/console/**").permitAll()
                 .anyRequest()
                 .authenticated()
